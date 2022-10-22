ae41dad9c545dd54c5291503087b97a8926c0392
SergiyVolo <voloshynsll@yahoo.com>
2022-10-22 09:52:01 -0400

    added two classes please create second student

---
+++ MavenProject/src/test/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Student student1 = new Student("John", 18);
+		System.out.println(student1.name);
+		student1.m1();
+
+	}
+
+}
